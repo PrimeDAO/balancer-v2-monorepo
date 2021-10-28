949f737f60ca36d7be94d41f2b4506728370751d
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-10-28 18:19:03 +0200

    tmp

--- pkg/pool-weighted/test/IndexPoolUtils.test.ts
+++ pkg/pool-weighted/test/IndexPoolUtils.test.ts
@@ -3,27 +3,35 @@ import { ethers } from 'hardhat';
 import { expect } from 'chai';
 import { Contract } from '@ethersproject/contracts';
 
+const HUNDRED_PERCENT = BigNumber.from(10).pow(18);
+
 const toBnPercentage = (decimalPercentage: number): BigNumber => {
   const normalizedWeight = decimalPercentage * 10e17;
   return BigNumber.from(normalizedWeight.toString());
 };
 
-const getExpectedWeights = (baseWeights: number[], fixWeights: number[]): BigNumber[] => {
-  let totalDenormalizedBaseWeight = 0;
-  let totalDenormalizedFixedWeight = 0;
+const getExpectedWeights = (baseWeightsNumbers: number[], fixWeightsNumbers: number[]): BigNumber[] => {
+  const baseWeights = baseWeightsNumbers.map((w) => toBnPercentage(w));
+  const fixWeights = fixWeightsNumbers.map((w) => toBnPercentage(w));
+
+  let totalDenormalizedBaseWeight = BigNumber.from(0);
+  let totalDenormalizedFixedWeight = BigNumber.from(0);
 
   for (let i = 0; i < baseWeights.length; i++) {
-    totalDenormalizedFixedWeight += fixWeights[i];
-    if (fixWeights[i] == 0) totalDenormalizedBaseWeight += baseWeights[i];
+    totalDenormalizedFixedWeight = totalDenormalizedFixedWeight.add(fixWeights[i]);
+    if (fixWeights[i].isZero()) {
+      totalDenormalizedBaseWeight = totalDenormalizedBaseWeight.add(baseWeights[i]);
+    }
   }
 
-  const sign = totalDenormalizedBaseWeight + totalDenormalizedFixedWeight > 1 ? -1 : 1;
-  const delta = Math.abs(1 - totalDenormalizedBaseWeight - totalDenormalizedFixedWeight);
+  const sign = totalDenormalizedBaseWeight.add(totalDenormalizedFixedWeight).gt(HUNDRED_PERCENT) ? -1 : 1;
+  const delta = HUNDRED_PERCENT.sub(totalDenormalizedBaseWeight).sub(totalDenormalizedFixedWeight).abs();
 
-  const finalWeights = baseWeights.map((initialBaseWeight: number, idx: number) => {
-    if (fixWeights[idx] != 0) return toBnPercentage(fixWeights[idx]);
+  const finalWeights = baseWeights.map((initialBaseWeight: BigNumber, idx: number) => {
+    if (!fixWeights[idx].isZero()) return fixWeights[idx];
 
-    const adjustedBaseWeight = initialBaseWeight + sign * (initialBaseWeight / totalDenormalizedBaseWeight) * delta;
+    // const adjustedBaseWeight = initialBaseWeight + sign * (initialBaseWeight / totalDenormalizedBaseWeight) * delta;
+    const adjustedBaseWeight = sign < 0 ? initialBaseWeight.add((initialBaseWeight / totalDenormalizedBaseWeight) * delta)
 
     return toBnPercentage(adjustedBaseWeight);
   });
@@ -31,7 +39,30 @@ const getExpectedWeights = (baseWeights: number[], fixWeights: number[]): BigNum
   return finalWeights;
 };
 
-describe.only('IndexPoolUtils', function () {
+// const getExpectedWeights = (baseWeights: number[], fixWeights: number[]): BigNumber[] => {
+//   let totalDenormalizedBaseWeight = 0;
+//   let totalDenormalizedFixedWeight = 0;
+
+//   for (let i = 0; i < baseWeights.length; i++) {
+//     totalDenormalizedFixedWeight += fixWeights[i];
+//     if (fixWeights[i] == 0) totalDenormalizedBaseWeight += baseWeights[i];
+//   }
+
+//   const sign = totalDenormalizedBaseWeight + totalDenormalizedFixedWeight > 1 ? -1 : 1;
+//   const delta = Math.abs(1 - totalDenormalizedBaseWeight - totalDenormalizedFixedWeight);
+
+//   const finalWeights = baseWeights.map((initialBaseWeight: number, idx: number) => {
+//     if (fixWeights[idx] != 0) return toBnPercentage(fixWeights[idx]);
+
+//     const adjustedBaseWeight = initialBaseWeight + sign * (initialBaseWeight / totalDenormalizedBaseWeight) * delta;
+
+//     return toBnPercentage(adjustedBaseWeight);
+//   });
+
+//   return finalWeights;
+// };
+
+describe('IndexPoolUtils', function () {
   let normalizerInstance: Contract;
 
   beforeEach(async () => {
@@ -71,6 +102,21 @@ describe.only('IndexPoolUtils', function () {
           expect(receivedWeights).to.eql(expectedWeights);
         });
       });
+
+      // describe('with crazy pool', () => {
+      //   const baseWeights = [0.341, 0.362, 0.123412, 0.173588];
+      //   const fixedWeights = [0, 0, 0.01, 0.01];
+
+      //   it.only('returns the correct interpolated normalized weights', async () => {
+      //     const expectedWeights = getExpectedWeights(baseWeights, fixedWeights);
+      //     const receivedWeights = await normalizerInstance.normalizeInterpolated(
+      //       baseWeights.map((w) => toBnPercentage(w)),
+      //       fixedWeights.map((w) => toBnPercentage(w))
+      //     );
+
+      //     expect(receivedWeights).to.eql(expectedWeights);
+      //   });
+      // });
     });
 
     describe('with denormalized weights smoller than one', () => {
@@ -90,14 +136,15 @@ describe.only('IndexPoolUtils', function () {
       });
     });
 
-    // describe('with 60/20/20 pool to be transferred in a ?/?/0 pool, aka removing the last token', () => {
-    //   const baseWeights = [toBnPercentage(0.6), toBnPercentage(0.2), toBnPercentage(0.2)];
-    //   const fixedWeights = [0, 0, 0];
-    //   it('returns the correct interpolated normalized weights', async () => {
-    //     const expectedWeights = [toBnPercentage((0.6 / 0.9) * 0.1), toBnPercentage((0.3 / 0.9) * 0.1), 0];
-    //     const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
-    //     expect(receivedWeights).to.eql(expectedWeights);
-    //   });
-    // });
+    describe('with 60/20/20 pool to be transferred in a ?/?/0 pool, aka removing the last token', () => {
+      const baseWeights = [toBnPercentage(0.6), toBnPercentage(0.2), toBnPercentage(0.2)];
+      const fixedWeights = [0, 0, 0];
+
+      it('returns the correct interpolated normalized weights', async () => {
+        const expectedWeights = [toBnPercentage((0.6 / 0.9) * 0.1), toBnPercentage((0.3 / 0.9) * 0.1), 0];
+        const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
+        expect(receivedWeights).to.eql(expectedWeights);
+      });
+    });
   });
 });
