52e4954a53c59c4baada496896cb0e87e5819b81
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-10-28 17:26:23 +0200

    adds function to retrieve expected values

--- pkg/pool-weighted/test/IndexPoolUtils.test.ts
+++ pkg/pool-weighted/test/IndexPoolUtils.test.ts
@@ -8,6 +8,29 @@ const toBnPercentage = (decimalPercentage: number): BigNumber => {
   return BigNumber.from(normalizedWeight.toString());
 };
 
+const getExpectedWeights = (baseWeights: number[], fixWeights: number[]): BigNumber[] => {
+  let totalDenormalizedBaseWeight = 0;
+  let totalDenormalizedFixedWeight = 0;
+
+  for (let i = 0; i < baseWeights.length; i++) {
+    totalDenormalizedFixedWeight += fixWeights[i];
+    if (fixWeights[i] == 0) totalDenormalizedBaseWeight += baseWeights[i];
+  }
+
+  const sign = totalDenormalizedBaseWeight + totalDenormalizedFixedWeight > 1 ? -1 : 1;
+  const delta = Math.abs(1 - totalDenormalizedBaseWeight - totalDenormalizedFixedWeight);
+
+  const finalWeights = baseWeights.map((initialBaseWeight: number, idx: number) => {
+    if (fixWeights[idx] != 0) return toBnPercentage(fixWeights[idx]);
+
+    const adjustedBaseWeight = initialBaseWeight + sign * (initialBaseWeight / totalDenormalizedBaseWeight) * delta;
+
+    return toBnPercentage(adjustedBaseWeight);
+  });
+
+  return finalWeights;
+};
+
 describe.only('IndexPoolUtils', function () {
   let normalizerInstance: Contract;
 
@@ -18,59 +41,63 @@ describe.only('IndexPoolUtils', function () {
   });
 
   describe('#normalizeInterpolated', () => {
-    describe('two base weights one fix weight', () => {
+    describe('with denormalized weights greater than one', () => {
       describe('with 80/20 pool and new token to be added w/ 1%', () => {
-        const baseWeights = [toBnPercentage(0.8), toBnPercentage(0.2), 0];
-        const fixedWeights = [0, 0, toBnPercentage(0.01)];
+        const baseWeights = [0.8, 0.2, 0];
+        const fixedWeights = [0, 0, 0.01];
 
         it('returns the correct interpolated normalized weights', async () => {
-          const expectedWeights = [toBnPercentage(0.792), toBnPercentage(0.198), toBnPercentage(0.01)];
-          const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
+          const expectedWeights = getExpectedWeights(baseWeights, fixedWeights);
+          const receivedWeights = await normalizerInstance.normalizeInterpolated(
+            baseWeights.map((w) => toBnPercentage(w)),
+            fixedWeights.map((w) => toBnPercentage(w))
+          );
 
           expect(receivedWeights).to.eql(expectedWeights);
         });
       });
 
-      describe('with 60/30/10 pool to be transferred in a ?/?/1 pool', () => {
-        const baseWeights = [toBnPercentage(0.6), toBnPercentage(0.3), toBnPercentage(0.1)];
-        const fixedWeights = [0, 0, toBnPercentage(0.01)];
-
-        it('returns the correct interpolated normalized weights', async () => {
-          const expectedWeights = [toBnPercentage(0.66), toBnPercentage(0.33), toBnPercentage(0.01)];
-          const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
-          expect(receivedWeights).to.eql(expectedWeights);
-        });
-      });
-    });
-
-    describe('two base weights two fix weights', () => {
       describe('with 80/20 pool and TWO new tokens to be added w/ 1%', () => {
-        const baseWeights = [toBnPercentage(0.8), toBnPercentage(0.2), 0, 0];
-        const fixedWeights = [0, 0, toBnPercentage(0.01), toBnPercentage(0.01)];
+        const baseWeights = [0.8, 0.2, 0, 0];
+        const fixedWeights = [0, 0, 0.01, 0.01];
 
         it('returns the correct interpolated normalized weights', async () => {
-          const expectedWeights = [
-            toBnPercentage(0.784),
-            toBnPercentage(0.196),
-            toBnPercentage(0.01),
-            toBnPercentage(0.01),
-          ];
-          const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
+          const expectedWeights = getExpectedWeights(baseWeights, fixedWeights);
+          const receivedWeights = await normalizerInstance.normalizeInterpolated(
+            baseWeights.map((w) => toBnPercentage(w)),
+            fixedWeights.map((w) => toBnPercentage(w))
+          );
 
           expect(receivedWeights).to.eql(expectedWeights);
         });
       });
-
-      // describe('with 60/30/10 pool to be transferred in a ?/?/1 pool', () => {
-      //   const baseWeights = [toBnPercentage(0.6), toBnPercentage(0.3), toBnPercentage(0.1)];
-      //   const fixedWeights = [0, 0, toBnPercentage(0.01)];
-
-      //   it('returns the correct interpolated normalized weights', async () => {
-      //     const expectedWeights = [toBnPercentage(0.66), toBnPercentage(0.33), toBnPercentage(0.01)];
-      //     const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
-      //     expect(receivedWeights).to.eql(expectedWeights);
-      //   });
-      // });
     });
+
+    describe('with denormalized weights smoller than one', () => {
+      describe('with 60/30/10 pool to be transferred in a ?/?/1 pool', () => {
+        const baseWeights = [0.6, 0.3, 0.1];
+        const fixedWeights = [0, 0, 0.01];
+
+        it('returns the correct interpolated normalized weights', async () => {
+          const expectedWeights = getExpectedWeights(baseWeights, fixedWeights);
+          const receivedWeights = await normalizerInstance.normalizeInterpolated(
+            baseWeights.map((w) => toBnPercentage(w)),
+            fixedWeights.map((w) => toBnPercentage(w))
+          );
+
+          expect(receivedWeights).to.eql(expectedWeights);
+        });
+      });
+    });
+
+    // describe('with 60/20/20 pool to be transferred in a ?/?/0 pool, aka removing the last token', () => {
+    //   const baseWeights = [toBnPercentage(0.6), toBnPercentage(0.2), toBnPercentage(0.2)];
+    //   const fixedWeights = [0, 0, 0];
+    //   it('returns the correct interpolated normalized weights', async () => {
+    //     const expectedWeights = [toBnPercentage((0.6 / 0.9) * 0.1), toBnPercentage((0.3 / 0.9) * 0.1), 0];
+    //     const receivedWeights = await normalizerInstance.normalizeInterpolated(baseWeights, fixedWeights);
+    //     expect(receivedWeights).to.eql(expectedWeights);
+    //   });
+    // });
   });
 });
