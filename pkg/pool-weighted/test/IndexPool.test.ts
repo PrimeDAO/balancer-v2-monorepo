62bea8637dba320a0198d8f4e6b33bc4c0d1e957
fabianschu <48454910+fabianschu@users.noreply.github.com>
2021-11-10 13:49:52 +0100

    sets up basic non-revert test case

--- pkg/pool-weighted/test/IndexPool.test.ts
+++ pkg/pool-weighted/test/IndexPool.test.ts
@@ -1,6 +1,6 @@
 import { ethers } from 'hardhat';
 import { expect } from 'chai';
-import { BigNumber } from 'ethers';
+import { BigNumber, Transaction } from 'ethers';
 import { range } from 'lodash';
 import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/dist/src/signer-with-address';
 import { fp, pct } from '@balancer-labs/v2-helpers/src/numbers';
@@ -352,6 +352,7 @@ describe('IndexPool', function () {
     context('when adding one new token', () => {
       const numberNewTokens = 1;
       const numberExistingTokens = 3;
+      const indexOfNewToken = 3;
       const newTokenTargetWeight = fp(0.1);
       const originalWeights = [fp(0.4), fp(0.3), fp(0.3)];
       const reindexWeights = [fp(0.5), fp(0.2), fp(0.2), newTokenTargetWeight];
@@ -437,7 +438,7 @@ describe('IndexPool', function () {
             poolId,
             kind: SwapKind.GivenIn,
             assetIn: reindexTokens[0],
-            assetOut: reindexTokens[3],
+            assetOut: reindexTokens[indexOfNewToken],
             amount: fp(0.001),
             userData: '0x',
           };
@@ -453,6 +454,35 @@ describe('IndexPool', function () {
           await expect(vault.instance.connect(owner).swap(singleSwap, funds, limit, deadline)).to.be.revertedWith(
             'Uninitialized token'
           );
+        });
+      });
+
+      context.only('when swapping new token into the pool', () => {
+        let swapTransaction: Transaction;
+
+        sharedBeforeEach('swap token into pool', async () => {
+          const singleSwap = {
+            poolId,
+            kind: SwapKind.GivenIn,
+            assetIn: reindexTokens[indexOfNewToken],
+            assetOut: reindexTokens[0],
+            amount: fp(0.001),
+            userData: '0x',
+          };
+          const funds = {
+            sender: owner.address,
+            fromInternalBalance: false,
+            recipient: other.address,
+            toInternalBalance: false,
+          };
+          const limit = 0; // Minimum amount out
+          const deadline = MAX_UINT256;
+
+          swapTransaction = vault.instance.connect(owner).swap(singleSwap, funds, limit, deadline);
+        });
+
+        it('does not revert', async () => {
+          await expect(swapTransaction).to.not.be.reverted;
         });
       });
     });
